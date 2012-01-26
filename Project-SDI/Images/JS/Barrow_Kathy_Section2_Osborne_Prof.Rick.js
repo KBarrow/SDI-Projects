23db591de99adafab223ee7c499eebe552baa6d0
KathyBarrow <Kbarrow@fullsail.edu>
2012-01-26 11:09:55 -0600

    -a -m

--- Project-SDI/Images/JS/Barrow_Kathy_Section2_Osborne_Prof.Rick.js
+++
@@ -1,7 +0,0 @@
-// Project 1 -Section 2
-// Professor: Rick Osborne
-// Aurthor: Kathy Barrow
-// Created For: SDI Online
-// Introductory look into Javascript using the Firebug console.
-
-console.log(true);
